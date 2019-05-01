dc06b3fe8f9de165853facf07a9c31b9b3aacefe
BILLY ENDRES <billyendres@BILLYs-MacBook-Pro.local>
2019-05-01 10:03:36 +1000

    connected 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        accessToken: process.env.CONTENFUL_ACCESS_TOKEN,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         spaceId: process.env.CONTENTFUL_SPACE_ID,
       },
     },
